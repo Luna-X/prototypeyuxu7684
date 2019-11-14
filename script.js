9658675e0e8b7c8d9a7075ede1e367969765b3e1
Luna-X <57375059+Luna-X@users.noreply.github.com>
2019-11-14 21:35:54 +1100

    beta ver1.9
    ready to work code (?)

---
+++ script.js
@@ -0,0 +1,8 @@
+var infotext = "Your password should contain letters and numbers.";
+console.log(1);
+
+function pwinfo() {
+    console.log(2);
+    document.getElementById("info").innerHTML = infotext;
+    return True
+}
