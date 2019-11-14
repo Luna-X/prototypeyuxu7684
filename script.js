ce02e35d5b1190dcdf59755e3543656ba1dd5ab1
Luna-X <57375059+Luna-X@users.noreply.github.com>
2019-11-14 22:20:23 +1100

    beta ver2.0
    appearance fixed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var infotext = "Your password should contain letters and numbers.";
 console.log(1);
-
+//The text should appear on the page when the function is called
 function pwinfo() {
     console.log(2);
     document.getElementById("info").innerHTML = infotext;
